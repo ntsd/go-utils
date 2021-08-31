0b42d2680918bd0784b982895d8619ab6757306a
Jirawat Boonkumnerd <jo06942@gmail.com>
2021-09-01 02:31:09 +0700

    feat(reflect): add to map string

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/ntsd/go-utils
+
+go 1.16
---
+++ pkg/reflect/to_map_test.go
@@ -0,0 +1,151 @@
+package reflect
+
+import (
+	"reflect"
+	"testing"
+
+	"github.com/ntsd/go-utils/pkg/pointer"
+)
+
+type TestStruct struct {
+	TestString        string  `form:"string,limit=10"`
+	TestPointerString *string `form:"pointerString,required"`
+	TestInt           int     `form:"int"`
+	TestPointerInt    *int    `form:"pointerInt"`
+}
+
+func TestToMapString(t *testing.T) {
+	type args struct {
+		in  interface{}
+		tag string
+	}
+	tests := []struct {
+		name    string
+		args    args
+		want    map[string]string
+		wantErr bool
+	}{
+		{
+			name: "success struct",
+			args: args{
+				in: TestStruct{
+					TestString:        "foo",
+					TestPointerString: pointer.ToString("bar"),
+					TestInt:           10,
+					TestPointerInt:    pointer.ToInt(15),
+				},
+				tag: "form",
+			},
+			want: map[string]string{
+				"string":        "foo",
+				"pointerString": "bar",
+				"int":           "10",
+				"pointerInt":    "15",
+			},
+			wantErr: false,
+		},
+		{
+			name: "success pointer struct",
+			args: args{
+				in: &TestStruct{
+					TestString:        "foo",
+					TestPointerString: pointer.ToString("bar"),
+					TestInt:           10,
+					TestPointerInt:    pointer.ToInt(15),
+				},
+				tag: "form",
+			},
+			want: map[string]string{
+				"string":        "foo",
+				"pointerString": "bar",
+				"int":           "10",
+				"pointerInt":    "15",
+			},
+			wantErr: false,
+		},
+		{
+			name: "success without string",
+			args: args{
+				in: &TestStruct{
+					TestPointerString: pointer.ToString("bar"),
+					TestInt:           10,
+					TestPointerInt:    pointer.ToInt(15),
+				},
+				tag: "form",
+			},
+			want: map[string]string{
+				"string":        "",
+				"pointerString": "bar",
+				"int":           "10",
+				"pointerInt":    "15",
+			},
+			wantErr: false,
+		},
+		{
+			name: "success without pointer string",
+			args: args{
+				in: &TestStruct{
+					TestString:        "foo",
+					TestPointerString: nil,
+					TestInt:           10,
+					TestPointerInt:    pointer.ToInt(15),
+				},
+				tag: "form",
+			},
+			want: map[string]string{
+				"string":     "foo",
+				"int":        "10",
+				"pointerInt": "15",
+			},
+			wantErr: false,
+		},
+		{
+			name: "success without int",
+			args: args{
+				in: &TestStruct{
+					TestString:        "foo",
+					TestPointerString: pointer.ToString("bar"),
+					TestPointerInt:    pointer.ToInt(15),
+				},
+				tag: "form",
+			},
+			want: map[string]string{
+				"string":        "foo",
+				"pointerString": "bar",
+				"int":           "0",
+				"pointerInt":    "15",
+			},
+			wantErr: false,
+		},
+		{
+			name: "success without pointer int",
+			args: args{
+				in: &TestStruct{
+					TestString:        "foo",
+					TestPointerString: pointer.ToString("bar"),
+					TestInt:           10,
+					TestPointerInt:    nil,
+				},
+				tag: "form",
+			},
+			want: map[string]string{
+				"string":        "foo",
+				"pointerString": "bar",
+				"int":           "10",
+			},
+			wantErr: false,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			got, err := ToMapString(tt.args.in, tt.args.tag)
+			if (err != nil) != tt.wantErr {
+				t.Errorf("ToMapString() error = %v, wantErr %v", err, tt.wantErr)
+				return
+			}
+			if !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("ToMapString() got = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
